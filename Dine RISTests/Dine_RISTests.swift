83899e1b032c4c7a81f6499bca6aec632f054255
Naman Kedia <Naman@namans-macbook-pro.local>
2017-01-20 14:21:23 +0700

    Initial Commit

---
+++ Dine RISTests/Dine_RISTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dine_RISTests.swift
+//  Dine RISTests
+//
+//  Created by Naman Kedia on 1/20/17.
+//  Copyright © 2017 Ruamrudee International School. All rights reserved.
+//
+
+import XCTest
+@testable import Dine_RIS
+
+class Dine_RISTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
